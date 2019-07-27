93f12c816d349349b742a305488434d0251f0d60
Pooja Wagle <pooja.wagle@hexagon.com>
2019-07-26 21:13:00 -0500

    Nothing implemented, but base app runs

---
+++ app/src/main/java/com/example/charl/hexagonwellness/BlogAdvice.java
@@ -0,0 +1,14 @@
+package com.example.charl.hexagonwellness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlogAdvice extends NavigationActivityBase {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_advice);
+        super.onCreateDrawer();
+    }
+}
